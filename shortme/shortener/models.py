2e86204597ea358964252d7b982bb5241304dccc
Samuel Bancal <Samuel.Bancal@epfl.ch>
2016-11-07 15:15:49 +0100

    start the model

--- shortme/shortener/models.py
+++ shortme/shortener/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
