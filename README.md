b163080a2b4f0329f8b22c2e832fb9fa8790f625
Samuel Bancal <Samuel.Bancal@epfl.ch>
2016-11-07 15:38:46 +0100

    more in the model + migration

--- README.md
+++ README.md
@@ -18,4 +18,6 @@ python manage.py createsuperuser
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py migrate shortener --fake
 ~~~
--- shortme/shortener/models.py
+++ shortme/shortener/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)  # every time the entry is saved
+    timestamp = models.DateTimeField(auto_now_add=True)  # when entry was created
 
     def __str__(self):
         return str(self.url)
