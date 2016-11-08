eb2973f0af3efb76e78df22c4ceb5d93c2e70c06
Samuel Bancal <Samuel.Bancal@epfl.ch>
2016-11-08 17:15:49 +0100

    Now start to redirect

--- shortme/shortener/views.py
+++ shortme/shortener/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
@@ -7,7 +7,7 @@ from .models import ShortURL
 # Create your views here.
 def short_redirect_view(request, shortcode=None, *args, **kwargs):  # Function Based View
     obj = get_object_or_404(ShortURL, shortcode=shortcode)
-    return HttpResponse("hello %s -> %s" % (shortcode, obj.url))
+    return HttpResponseRedirect(obj.url)
 
 class ShortCBView(View):  # Class Based View
     def get(self, request, shortcode=None, *args, **kwargs):
